9a106f01b6e4524a694ba4fb1cc4913697c95aa3
illiyasmahe <illiyas@alhodood.com>
2026-03-12 10:59:42 +0530

    added git ignore

--- ats_tracking/models/applicant_stage.py
+++ ats_tracking/models/applicant_stage.py
@@ -1,3 +1,24 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+
+#    Alhodood Technologies.
+#
+#    Copyright (C) 2026-TODAY Alhodood Technologies(<https://www.alhodood.com>)
+#    Author: Alhodood Technologies(<https://www.alhodood.com>)
+#
+#    You can modify it under the terms of the GNU Affero General Public License
+#    (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
 from odoo import models, fields
 
 class ApplicantStage(models.Model):
